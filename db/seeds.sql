ab7c049cd2f189b9a785b5d392fd190ce8ae4e02
Your Name <you@example.com>
2019-02-09 19:23:01 -0700

    initial commit

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers (burger_name) VALUES ('bacon cheeseburger');
+INSERT INTO burgers (burger_name) VALUES ('black and blue burger');
+INSERT INTO burgers (burger_name) VALUES ('southwest black bean burger');
+INSERT INTO burgers (burger_name) VALUES ('california burger');
+INSERT INTO burgers (burger_name) VALUES ('green chile burger');
